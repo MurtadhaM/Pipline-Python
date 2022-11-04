a44538e17428b7ee86dfd4214cf9aa455e4b967b
Murtadha Marzouq <mmarzou0@gmail.com>
2022-11-04 15:57:51 -0400

    😜

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-var express = require('express');  
-var app = express(8080);  
+/*
+Author: Murtadha Marzouq
+*/
+
+// Importing the required modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Creating an instance of express
+const app = express();
+
+// Setting up the port
+const port = process.env.PORT || 8080;
+
+// Setting up the middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+// Setting up the routes
+app.use('/api', (req, res) => {
+   
+      // Getting the data from the request body
+      const data = req.body;
+
+      // Sending the data back to the client
+      res.send(data);
+});
 
 
 
+// Listening to the port
+app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+   }
+);
 
 
-
-app.get('/', function (req, res) {  
-   res.send('Welcome to JavaTpoint');  
-})  
-
-
-
+app.get('/', (req, res) => {
+   // returning the index.html file
+   res.sendFile(__dirname + '/templates/index.html');
+});
 
 
 
--- tempCodeRunnerFile.ps1
+++
@@ -1,6 +0,0 @@
-foreach($row in $2Darray){
-        foreach($col in $row){
-            Write-Host -Message $col -ForegroundColor Green
-        }
-        Write-Host -Message ' ' -ForegroundColor Green
-    }
